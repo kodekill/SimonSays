da20d9b0c7270cbba4c610eeedbdffb21483e2a4
kodekill <brian.falor@gmail.com>
2018-04-24 21:40:19 -0600

    First Commit

---
+++ Simon.ino
@@ -0,0 +1,22 @@
+#define LIGHT_1 2
+#define LIGHT_2 3
+#define LIGHT_3 4
+#define LIGHT_4 5
+
+#define BUTTON_1 8
+#define BUTTON_2 9
+#define BUTTON_3 10
+#define BUTTON_4 11
+
+#define PIEZO 13
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
