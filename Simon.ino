c30a7938c9b22f591930aa42d2b48c8874adc2ed
kodekill <brian.falor@gmail.com>
2018-04-25 21:46:33 -0600

    Implemented a stack and got some sounds and lights to go off when the stack is parsed through

--- Simon.ino
+++ Simon.ino
@@ -1,15 +1,23 @@
+#include <StackList.h>
+
 #define LIGHT_1 2    //Red
 #define LIGHT_2 3    //Yellow
-#define LIGHT_3 4    //White
-#define LIGHT_4 5    //Red
+#define LIGHT_3 4    //White    //Green
+#define LIGHT_4 5    //Red      //Blue
 
 #define BUTTON_1 8   //Red
 #define BUTTON_2 9   //Yellow
-#define BUTTON_3 10  //White
-#define BUTTON_4 11  //Red
+#define BUTTON_3 10  //White    //Green
+#define BUTTON_4 11  //Red      //Blue
 
 #define PIEZO 12
-int delayTime = 750; 
+
+StackList<int> stack;
+
+int delayTime = 750;
+int MAX_ARRAY = 50;
+int myArray[50];
+
 
 void setup() {
   pinMode(LIGHT_1, OUTPUT);
@@ -23,9 +31,79 @@ void setup() {
   pinMode(BUTTON_4, INPUT);
   
   Serial.begin(9600);
+    addStack();
 }
 
+
 void loop() {
+
+  //addStack();
+  readStack();
+
+}
+
+int addStack(){
+  
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+
+}
+
+
+void readStack(){
+  if(stack.isEmpty()){
+    Serial.println("true");
+  }
+  int x = stack.pop();
+  lightSound(x);
+
+}
+
+
+void lightSound(int x){
+  switch (x){
+    case 1:
+      digitalWrite(LIGHT_1, HIGH);
+      tone(PIEZO, 3000);
+      delay(delayTime);
+      lightSoundOff();
+      break;
+
+    case 2:
+      digitalWrite(LIGHT_2, HIGH);
+      tone(PIEZO, 4000);
+      delay(delayTime);
+      lightSoundOff();
+      break;
+
+    case 3:
+      digitalWrite(LIGHT_3, HIGH);
+      tone(PIEZO, 5000);
+      delay(delayTime);
+      lightSoundOff();
+      break;
+
+    case 4:
+      digitalWrite(LIGHT_4, HIGH);
+      tone(PIEZO, 6000);
+      delay(delayTime);
+      lightSoundOff();   
+      break;
+  } 
+}
+
+void lightSoundOff(){
+  digitalWrite(LIGHT_1, LOW);
+  digitalWrite(LIGHT_2, LOW);
+  digitalWrite(LIGHT_3, LOW);
+  digitalWrite(LIGHT_4, LOW);
+
+  noTone(PIEZO);
+  delay(100);
+}
+
+void test() {
   digitalWrite(LIGHT_1, HIGH);
   digitalWrite(LIGHT_2, HIGH);
   digitalWrite(LIGHT_3, HIGH);
@@ -70,6 +148,8 @@ void loop() {
     delay(delayTime);
     noTone(PIEZO);
   }
-
-   
+  
 }
+
+
+
