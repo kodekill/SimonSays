0bbf6bf6832801fd49dce24c40ed65c13d413cfb
kodekill <brian.falor@gmail.com>
2018-05-01 22:26:17 -0600

    Worked on adding a reset, and input check.

--- Simon.ino
+++ Simon.ino
@@ -13,11 +13,16 @@
 #define PIEZO 12
 
 StackList<int> stack;
+StackList<int> Rstack; //read stack
+StackList<int> copy;
 
-int delayTime = 750;
+int delayTime = 375;
 int MAX_ARRAY = 50;
 int myArray[50];
-int roundScore = 1;
+int roundScore = 1; //How many iterations made
+int myCount = 0;    //Used for Stack Length
+int input = 0; 
+float mySpeed = 2;
 bool checkFlag = false;
 
 
@@ -37,29 +42,137 @@ void setup() {
   stack.push(1);
   stack.push(2);
   stack.push(3);
+  successInput();
 }
 
+void(* resetFunc) (void) = 0; //Reset
 
 void loop() {
+  //Serial.println("\n\nLoop");
   //addStack();
+  
+  //Serial.println(stack.peek());
   //readStack();
-  answer();
+
+  //Serial.print("My input = ");
+  //Serial.println(input);
+
+  buttonPressCheck();
+  while(checkFlag == true){
+    answer();
+  }
 
   roundScore++;
 }
 
-void addStack(){
-  for(int j = 0; j < roundScore; j++){
-    int myRandom = random(1, 5);
-    stack.push(myRandom);
+
+
+
+
+void answer(){
+  int compareItem = stack.pop();
+  input = buttonPressCheck();
+
+  while(input != 0){
+    if(compareItem == input){
+      //Serial.println("You got it right"); 
+      checkFlag = false;
+      input = 0;
+      break;
+    }
+    if(compareItem != input){
+      //Serial.println("You got it wrong");
+      checkFlag = false;
+      input = 0;
+      failInput();
+    }
+    break;
   }
 }
 
 
+int buttonPressCheck(){
+  int button1state = digitalRead(BUTTON_1);
+  int button2state = digitalRead(BUTTON_2);
+  int button3state = digitalRead(BUTTON_3);
+  int button4state = digitalRead(BUTTON_4);
+  mySpeed = 1.2; 
+
+  if(button1state == true){
+    lightSound(1, mySpeed);
+    input = 1;
+    checkFlag = true;
+  }
+  if(button2state == true){
+    lightSound(2, mySpeed);
+    input = 2;
+    checkFlag = true;
+  }
+  if(button3state == true){
+    lightSound(3, mySpeed);
+    input = 3;
+    checkFlag = true;
+  }  
+  if(button4state == true){
+    lightSound(4, mySpeed);
+    input = 4;
+    checkFlag = true;
+  }
+//  Serial.print("input = ");
+//  Serial.println(input);
+//  
+//  Serial.print("checkFlag = ");
+//  Serial.println(checkFlag);
+  
+  return input;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void addStack(){
+    int myRandom = random(1,5);
+    stack.push(myRandom);
+    Serial.print("Pushing ");
+    Serial.print(myRandom);
+    Serial.println(" to the stack");
+    copy = stack;
+    checkFlag = true;
+}
+
+
 void readStack(){
-  while(!stack.isEmpty()){
-      int x = stack.pop();
-      lightSound(x);
+  Rstack = copy;
+  myCount = Rstack.count();
+  Serial.print("My stack size = ");
+  Serial.println(myCount);
+  
+  for(int i = 0; i < myCount; i++){
+      int x = Rstack.pop();
+      Serial.print("Popping ");
+      Serial.print(x);
+      Serial.println(" off");
+      lightSound(x, mySpeed);
   }
   Serial.println("stack is now empty");
   lightAll();
@@ -74,67 +187,65 @@ void failInput(){
     digitalWrite(LIGHT_3, HIGH);
     digitalWrite(LIGHT_4, HIGH);
     tone(PIEZO, 2000); 
-    delay(500);
+    delay(300);
 
     digitalWrite(LIGHT_1, LOW);
     digitalWrite(LIGHT_2, LOW);
     digitalWrite(LIGHT_3, LOW);
     digitalWrite(LIGHT_4, LOW);
     noTone(PIEZO);
-    delay(300);
+    delay(150);
   }
+  resetFunc();
 }
 
 
-void lightSound(int x){
+void successInput(){
+  float mySpeed = .25;
+
+  for(int i = 1; i <= 4; i++){
+    lightSound(i, mySpeed);
+  }
+  for(int i = 3; i >= 2; i--){
+    lightSound(i, mySpeed);
+  }
+  lightSound(1, mySpeed*2.5);
+}
+
+
+void lightSound(int x, float mySpeed){
   switch (x){
     case 1:
       digitalWrite(LIGHT_1, HIGH);
       tone(PIEZO, 3000);
-      delay(delayTime);
+      delay(delayTime * mySpeed);
       lightSoundOff();
       break;
 
     case 2:
       digitalWrite(LIGHT_2, HIGH);
       tone(PIEZO, 4000);
-      delay(delayTime);
+      delay(delayTime * mySpeed);
       lightSoundOff();
       break;
 
     case 3:
       digitalWrite(LIGHT_3, HIGH);
       tone(PIEZO, 5000);
-      delay(delayTime);
+      delay(delayTime * mySpeed);
       lightSoundOff();
       break;
 
     case 4:
       digitalWrite(LIGHT_4, HIGH);
       tone(PIEZO, 6000);
-      delay(delayTime);
+      delay(delayTime * mySpeed);
       lightSoundOff();   
       break;
   } 
 }
 
 
-void answer(){
-  int button1state = digitalRead(BUTTON_1);
-  int button2state = digitalRead(BUTTON_2);
-  int button3state = digitalRead(BUTTON_3);
-  int button4state = digitalRead(BUTTON_4);
-
-  int compareItem = stack.pop();
-
-  if()
-
-  
-}
-
-void buttonPressCheck(){
-
-}
 
 
 
